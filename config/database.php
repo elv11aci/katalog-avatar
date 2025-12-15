69b5e8f8ec294ffe046af11eb4c242aa4cb544ba
aciiielv <astiaulia13@gmail.com>
2025-12-15 10:35:20 +0700

    new

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+class Database{
+    private $connection;
+
+    public function getConnection(){
+        $this->connection = null;
+        try{
+            // mysqli(namaserver, username_database, password_database, nama_database)
+            $this->connection = new mysqli("localhost", "root", '', "db_katalog-avatar");
+
+            if($this->connection->connect_error){
+                throw new Exception($this->connection->connect_error);
+            }
+
+            $this->connection->set_charset("utf8");
+            return $this->connection;
+        }catch(Exception $e){
+            echo 'Connection failed : '.$e->getMessage();
+        }
+    }
+}
